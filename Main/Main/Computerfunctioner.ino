6ad548cfe85d04257d74d0010e0dbbecafc58827
Moerup42 <jacob@rmcm.dk>
2021-12-02 14:04:35 +0100

    bibber ved timerne

--- Main/Main/Computerfunctioner.ino
+++ Main/Main/Computerfunctioner.ino
@@ -48,6 +48,26 @@ void DitUr() {
   lcd.print("/");
   lcd.print(clock.year + 2000, DEC);
   lcd.print(" ");
+  
+  if (clock.second == 0) {
+    for (i = 0; i < 6; i ++) {
+      if (clock.hour == tid[i][0] && clock.minute == tid[i][2]) {
+        digitalWrite(2, 1);
+        delay(1000);
+        digitalWrite(2, 0);
+//        break;
+      }else if (clock.hour == tid[i][1] && clock.minute == tid[i][2]) {
+        digitalWrite(2, 1);
+        delay(400);
+        digitalWrite(2, 0);
+        delay(200);
+        digitalWrite(2, 1);
+        delay(400);
+        digitalWrite(2, 0);
+//        break;
+      }
+    }
+  }
 }
 
 
@@ -56,7 +76,7 @@ void getName(void) {
   lcd.print(RandomElev[navnNr]);
   if (digitalRead(A1) && tryk){
     clock.getTime();
-    for (uint8_t i = 0; i < clock.second; i ++) {
+    for (i = 0; i < clock.second; i ++) {
       navnNr = random(0, 30);
     }
     tryk = false;
@@ -87,7 +107,7 @@ void StopUr() {
 }
 
 
-void Christsmas() {
+void Christmas() {
   lcd.setCursor(0, 0);
   lcd.print(12 - clock.month);
   lcd.print(" maaned(er) og");
